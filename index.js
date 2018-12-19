0ceb623331d02e9f5ae386f9adc269b0b8c921d2
Rafael Abensur <rafael@abensur.me>
2018-12-19 03:34:17 -0200

    v1.0.0

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const chalk 		= require("chalk");
+const github  		= require('./lib/github');
+
+const init = () => {
+	console.log(chalk.green("etiquette"));
+};
+
+const success = () => {
+	console.log(chalk.white.bgGreen.bold(`Done!`));
+};
+
+const run = async () => {
+	init();
+
+	let token = github.getStoredGithubToken();
+
+	if (!token) {
+		await github.setGithubCredentials();
+		await github.registerNewToken();
+	}
+
+	const deps = await github.flattenDeps();
+
+	if (!deps.length) {
+		return console.log(chalk.red('No dependencies found'));
+	}
+
+	await github.starRepos(deps);
+	success();
+};
+
+run();
